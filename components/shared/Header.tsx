1ddbf06e6ac9187c0eda1a4558db22add91e9c55
AliHamza0007 <141808379+AliHamza0007@users.noreply.github.com>
2024-06-05 14:31:01 +0500

    make route to naviugater

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,18 +1,30 @@
-import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
-import Image from "next/image"
-import Link from "next/link"
-import { Button } from "../ui/button"
-import NavItems from "./NavItems"
-import MobileNav from "./MobileNav"
+import { SignedIn, SignedOut, UserButton, useUser } from '@clerk/nextjs';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from '../ui/button';
+import NavItems from './NavItems';
+import MobileNav from './MobileNav';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 const Header = () => {
+  const { user } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      router.replace('/');
+    }
+  }, [user, router]);
   return (
     <header className="w-full border-b">
       <div className="wrapper flex items-center justify-between">
         <Link href="/" className="w-36">
-          <Image 
-            src="/assets/images/logo.svg" width={128} height={38}
-            alt="Evently logo" 
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="Evently logo"
           />
         </Link>
 
@@ -29,15 +41,13 @@ const Header = () => {
           </SignedIn>
           <SignedOut>
             <Button asChild className="rounded-full" size="lg">
-              <Link href="/sign-in">
-                Login
-              </Link>
+              <Link href="/sign-in">Login</Link>
             </Button>
           </SignedOut>
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
