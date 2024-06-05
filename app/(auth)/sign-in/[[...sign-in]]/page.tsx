86bad1a986ce80f194ef427a41b34682d4f0ffe9
AliHamza0007 <141808379+AliHamza0007@users.noreply.github.com>
2024-06-05 13:10:14 +0500

    Update page.tsx

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn fallbackRedirectUrl="/dashboard" signUpFallbackRedirectUrl="/onboarding"  />
+  return <SignIn fallbackRedirectUrl="/" signUpFallbackRedirectUrl="/"  />
 }
